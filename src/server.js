98fe8441affd9e7f970c5cfdc1bd318fc9966f3b
cooleo273 <leulteferi273@gmail.com>
2024-10-15 18:46:04 +0300

    adding supabase authentication

--- src/server.js
+++ src/server.js
@@ -15,10 +15,7 @@
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     // Enable CORS for all routes
-    app.use(cors({
-        origin: 'http://localhost:3000', // Allow requests from this origin
-    }));
-
+    app.use(cors({ origin: ["https://atlanticlubes-n46j.vercel.app", "http://localhost:3000"] }));
     app.use(bodyParser.json()); // For parsing application/json
 
     // Base URL for inventory routes
